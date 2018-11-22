65cca5abc2fc71ef002a5a16939570483f02adc9
Patrick Apgar <papgar9@gmail.com>
2018-11-22 16:18:32 +0000

    installed create-react-app and other modules as well as updated node version to 8. Began project by making changes to ./src/App.js and creating new file that hides text. Left off at creating toggleIsHidden method to hide text, will continue later.

---
+++ my-first-app/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import HideableText from './HideableText';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello World!! :)
+            <HideableText text = "Dynamic Text!"/>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
