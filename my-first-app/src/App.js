2eef0256b4c513834f94e246d374a2266176dc7a
Patrick Apgar <papgar9@gmail.com>
2018-11-27 18:24:54 +0000

    Created Suggestion selection function in AutoCompleteText component. Made additional changes to component as well as AutoCompleteText.css file

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
 import AutoCompleteText from './AutoCompleteText';
 
 
--- my-first-app/src/AutoCompleteText.js
+++ my-first-app/src/AutoCompleteText.js
@@ -1,5 +1,7 @@
-// You must ALWAYS import React modules in every component you build!
+// REMINDER: must ALWAYS import React modules in every component you build!
 import React from 'react';
+// import AutoCompleteText.css
+import './AutoCompleteText.css'; 
 
 // Write comment here about AutoCompleteText class
 export default class AutoCompleteText extends React.Component {
@@ -12,11 +14,12 @@ export default class AutoCompleteText extends React.Component {
                 'Jane',
             ];
             // we must use this.state to show the text we entered outside of the JS console
-            // the term "state" is simply internal data belonging to an instance of a the component it refers to
+            // the term "state" is simply internal data belonging to an instance of the component it refers to
             // this.state is a special property within react and, in this case, refers to
             // the "this" keyword
             this.state = {
                 suggestions: [],
+                text: '',
             }
     }
     
@@ -35,7 +38,17 @@ export default class AutoCompleteText extends React.Component {
             // And then filter so to match the value of regex
             suggestions = this.items.sort().filter(v => regex.test(v));
         }
-        this.setState(() => ({ suggestions }));
+        this.setState(() => ({ suggestions, text: value }));
+    }
+    
+    // Below I've created the function 'SuggestionSelected' which will take the value of the 
+    // suggestion and will be one of the items from the suggestion list as an argument
+    SuggestionSelected (value) {
+        // here I use 'this.setState()' to ensure teh value the textbox uses is the selected suggestion
+        this.setState(() => ({
+            text: value,
+            suggestions: [],
+        }));
     }
     
     // Here I've created this function to render the suggested results that appear as the user types
@@ -46,16 +59,24 @@ export default class AutoCompleteText extends React.Component {
             return null;
         }
         return (
+                    // A bug occured here within this code that stated "cannot read map property of undefined"
+                    // Since 'suggestions' has already been destructured in the code above, it is now passed as 
+                    // a variable to be used within the rest of this code block. 
+                    // At first, the code below read 'this.suggestions.map()' however we only need 'suggestions' from the destructured variable above.
                     <ul>
-                        {suggestions.map((item) => <li>{item}</li>)}
+                        {suggestions.map((item) => <li onClick = {() => this.SuggestionSelected(item)}>{item}</li>)}
                     </ul>
                 )
     }
-
+    
     render () {
+    // render function
+    // destructure 'text' from "this.state" object
+    const { text } = this.state;
         return (
-                <div>
-                    <input onChange={this.onTextChanged} type = "text" />
+                // the value in the textbox is now assigned to the 'text' value of our state
+                <div className = "AutoCompleteText">
+                    <input value = {text} onChange={this.onTextChanged} type = "text" />
                     {this.renderSuggestions()}
                 </div>
             );
