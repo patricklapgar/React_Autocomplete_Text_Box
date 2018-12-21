552dfe2be3a54ca50a392bde2cc1ce83afcc2f46
Patrick Apgar <papgar9@gmail.com>
2018-12-21 07:34:51 +0000

    made more changes to the app including updates to component states within the Series component and added a new input form that has several features of displaying content.

--- reactCourse/tv-series-app/src/containers/Series/index.js
+++ reactCourse/tv-series-app/src/containers/Series/index.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
-
+import SeriesList from '../../components/SeriesList';
 
 class Series extends Component {
     
     state = {
-    series:[]
+    series:[],
+    seriesName: '',
+    isFetching: false
   }
   
-  componentDidMount () {
-    // fetch allows me to chain then methods to return the json format of our results from
-    // the response object
-    // API request here
-    window.fetch('https://api.tvmaze.com/search/shows?q=Vikings')
-    .then((response) => response.json())
-    .then(json => this.setState({ series: json }))
-  }
+    onSeriesInputChange = e => {
+        this.setState({ seriesName: e.target.value, isFetching: true});
+        // fetch allows me to chain then methods to return the json format of our results from
+        // the response object
+        // API request here
+        window.fetch(`https://api.tvmaze.com/search/shows?q=${e.target.value}`)
+        .then((response) => response.json())
+        .then(json => this.setState({ series: json, isFetching: false }))
+    }
     
     render () {
+        const { series, seriesName, isFetching } = this.state;
         return (
                 <div className = "App-series">        
-                    The length of series array - {this.state.series.length}
+                    <div>
+                        <input 
+                            value = {seriesName} 
+                            type = "text" 
+                            onChange={this.onSeriesInputChange} />
+                    </div>
+                    { 
+                        series.length === 0 && seriesName.trim() === '' 
+                        &&
+                        <p>Please enter the series name into the input</p>
+                    }
+                    {
+                        !isFetching && series.length === 0 && seriesName.trim() !== ''
+                        &&
+                        <p>No TV series have been found with this name</p>
+                    }
+                    {
+                        isFetching && <p>loading...</p>
+                    }
+                    {
+                        !isFetching && <SeriesList list = {this.state.series} />
+                    }
                 </div>
-            )
+            );
     }
 }
 
